597e3b2a9064d4f82040656edf050d7e8ee84300
Linah02 <120464421+Linah02@users.noreply.github.com>
2025-02-07 20:51:58 +0300

    Update settings.py

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myproject.myapp',#integration de mon app dans mon projet
+    'myproject.myapp.apps.MyappConfig.name',#integration de mon app dans mon projet
     'django.contrib.humanize',
     'corsheaders',
 ]
