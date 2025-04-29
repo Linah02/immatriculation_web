2c5dfa3ebc1b0cb2b01ad2d65806bc9ab3e47c3b
Linah02 <120464421+Linah02@users.noreply.github.com>
2025-04-29 10:49:55 +0300

    ajour sidebar

--- myapp/static/myapp/database/data_declarationDE.sql
+++ myapp/static/myapp/database/data_declarationDE.sql
@@ -4,6 +4,8 @@ INSERT into myapp_taux_droit_enregistrement(type_droit, taux) VALUES
 ('Suceccion',10),
 ('Donation',10);
 
-INSERT into myapp_list_DE() VALUES
-()
-();
+INSERT into myapp_declaration(base_imposable,mnt_ap,date_declaration,id_contribuable_id,id_tde_id) VALUES
+(80000,10000,'2024-11-05',1,2);
+
+
+-- IA dans la message ___ repondeur automatique comme chat bot
